4c5634129cd36ae5aebd8cead56a2085c6bb2c8b
Ritesh Kumar <rk1972445@gmail.com>
2024-10-13 14:15:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  var typed = new Typed('#element', {
+    strings: ['Web-developer', 'UI/UX Designer','Data Analyst','Prompt Engineer'],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+  });
+
+ 
